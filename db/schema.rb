32c31531094e2ce7aa308a16fe9a1a2229ad4d4c
Ferdinando Traversa <ferdi.traversa@gmail.com>
2022-10-07 23:46:15 +0200

    Correggo tipo qid

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_150230) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_214544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "castles", force: :cascade do |t|
-    t.integer "qid"
+    t.string "qid"
     t.string "label"
     t.string "description"
     t.decimal "latitude"
