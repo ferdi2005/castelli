175b3c6f849c787a19a5a819ea356fbf37ec00ba
Ferdinando Traversa <ferdi.traversa@gmail.com>
2022-10-14 17:17:07 +0200

    Aggiungo indicatore posizione

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "leaflet", to: "leaflet/dist/leaflet.js", preload: true
 pin "leaflet.markercluster", to: "leaflet.markercluster/dist/leaflet.markercluster.js", preload: true
+pin "leaflet.locatecontrol", to: "leaflet.locatecontrol/dist/L.Control.Locate.min.js", preload: true
+
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,5 +5,4 @@ Sentry.init do |config|
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    config.traces_sample_rate = 1.0
   end
